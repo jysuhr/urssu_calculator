2c77b7e3b633ddc27a9a6c7c3c889722d393e148
jysuhr <jun0.suhr@gmail.com>
2024-05-02 02:51:57 +0900

    Add and Subtract Succed!

--- urssu_calculator/ViewController.swift
+++ urssu_calculator/ViewController.swift
@@ -31,11 +31,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var divide_button: UIButton!
     
     
+    @IBOutlet weak var ok_button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        info_lable.text = "버튼을 눌러주세요!"
+        //firstNum_textField.text = "첫번째 숫자를 입력해주세요" // 클릭하면 없어지는 글자여야돼는데...
+        
     }
-
-
+    
+    // 여기에 액션 연결
+    
+    @IBAction func add_action(_ sender: UIButton) {
+        let num1: Int? = Int(firstNum_textField.text!)
+        let num2: Int? = Int(secondNum_textField.text!)
+        var result: Int
+        result = num1! + num2!
+        info_lable.text = firstNum_textField.text! + " + " + secondNum_textField.text! + " = " + "\(result)"
+    }
+    
+    @IBAction func sub_action(_ sender: UIButton) {
+        let num1: Int? = Int(firstNum_textField.text!)
+        let num2: Int? = Int(secondNum_textField.text!)
+        var result: Int
+        result = num1! - num2!
+        info_lable.text = firstNum_textField.text! + " - " + secondNum_textField.text! + " = " + "\(result)"
+    }
+    
+    
+    
+    
 }
 
