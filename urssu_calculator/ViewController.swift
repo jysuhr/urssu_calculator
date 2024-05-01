55d7a6371ffdd20633d6a56aeba53d2daf782da9
jysuhr <jun0.suhr@gmail.com>
2024-05-02 02:04:57 +0900

    Layout_foundation

--- urssu_calculator/ViewController.swift
+++ urssu_calculator/ViewController.swift
@@ -8,7 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var firstNum_textField: UITextField!
+    
+    
+    @IBOutlet weak var secondNum_textField: UITextField!
+    
+    
+    @IBOutlet weak var info_lable: UILabel!
+    
+    
+    @IBOutlet weak var add_button: UIButton!
+    
+    
+    @IBOutlet weak var subtract_button: UIButton!
+    
+    
+    @IBOutlet weak var multiply_button: UIButton!
+    
+    
+    @IBOutlet weak var divide_button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
