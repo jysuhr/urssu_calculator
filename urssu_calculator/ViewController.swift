874bb8e32cdbab7b41ae54cec4a4752cc896385b
jysuhr <jun0.suhr@gmail.com>
2024-05-03 11:58:55 +0900

    사칙연산 두 값 모두 nil일 때와 한 값만 nil일 때의 처리 완료

--- urssu_calculator/ViewController.swift
+++ urssu_calculator/ViewController.swift
@@ -53,7 +53,7 @@ class ViewController: UIViewController {
         if let reNum1 = num1 { // 첫째 값은 있을 때
             if let reNum2 = num2 { // 둘째 값도 있을 때
                 result = reNum1 + reNum2
-                info_lable.text = "\(result)"
+                info_lable.text = firstNum_textField.text! + " + " + secondNum_textField.text! + " = " + "\(result)"
             }
             else {
                 info_lable.text = "숫자를 모두 입력해주세요"
@@ -71,12 +71,21 @@ class ViewController: UIViewController {
         let num1: Int? = Int(firstNum_textField.text!)
         let num2: Int? = Int(secondNum_textField.text!)
         var result: Int = 0
-        if let reNum1 = num1, let reNum2 = num2 {
-            result = reNum1 - reNum2
-            info_lable.text = firstNum_textField.text! + " - " + secondNum_textField.text! + " = " + "\(result)"
+        
+        if let reNum1 = num1 { // 첫째 값은 있을 때
+            if let reNum2 = num2 { // 둘째 값도 있을 때
+                result = reNum1 - reNum2
+                info_lable.text = firstNum_textField.text! + " - " + secondNum_textField.text! + " = " + "\(result)"
+            }
+            else {
+                info_lable.text = "숫자를 모두 입력해주세요"
+            }
+        }
+        else if let reNum2 = num2 {
+            info_lable.text = "숫자를 모두 입력해주세요"
         }
         else {
-            info_lable.text = "숫자를 모두 입력해주세요"
+            info_lable.text = "값을 먼저 입력해주세요"
         }
     }
     
@@ -86,12 +95,20 @@ class ViewController: UIViewController {
         let num2: Int? = Int(secondNum_textField.text!)
         var result: Int = 0
         
-        if let reNum1 = num1, let reNum2 = num2 {
-            result = reNum1 * reNum2
-            info_lable.text = firstNum_textField.text! + " X " + secondNum_textField.text! + " = " + "\(result)"
+        if let reNum1 = num1 { // 첫째 값은 있을 때
+            if let reNum2 = num2 { // 둘째 값도 있을 때
+                result = reNum1 * reNum2
+                info_lable.text = firstNum_textField.text! + " X " + secondNum_textField.text! + " = " + "\(result)"
+            }
+            else {
+                info_lable.text = "숫자를 모두 입력해주세요"
+            }
+        }
+        else if let reNum2 = num2 {
+            info_lable.text = "숫자를 모두 입력해주세요"
         }
         else {
-            info_lable.text = "숫자를 모두 입력해주세요"
+            info_lable.text = "값을 먼저 입력해주세요"
         }
     }
     
@@ -101,12 +118,20 @@ class ViewController: UIViewController {
         let num2: Int? = Int(secondNum_textField.text!)
         var result: Int = 0
         
-        if let reNum1 = num1, let reNum2 = num2 {
-            result = reNum1 / reNum2
-            info_lable.text = firstNum_textField.text! + " / " + secondNum_textField.text! + " = " + "\(result)"
+        if let reNum1 = num1 { // 첫째 값은 있을 때
+            if let reNum2 = num2 { // 둘째 값도 있을 때
+                result = reNum1 / reNum2
+                info_lable.text = firstNum_textField.text! + " / " + secondNum_textField.text! + " = " + "\(result)"
+            }
+            else {
+                info_lable.text = "숫자를 모두 입력해주세요"
+            }
+        }
+        else if let reNum2 = num2 {
+            info_lable.text = "숫자를 모두 입력해주세요"
         }
         else {
-            info_lable.text = "숫자를 모두 입력해주세요"
+            info_lable.text = "값을 먼저 입력해주세요"
         }
     }
     // 강제추출 옵셔널이라서 nil값이 들어오면 문제가 생김 -> guard-let or if-let 고려하기
