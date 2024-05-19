679cee0b794e4e08ef435cadf8edffa6f97bcaa5
jysuhr <jun0.suhr@gmail.com>
2024-05-20 02:02:54 +0900

    초기화면 구성

--- Assignment2-NotePad/Assignment2-NotePad/ViewController.swift
+++ Assignment2-NotePad/Assignment2-NotePad/ViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UINavigationController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +17,33 @@ class ViewController: UIViewController {
 
 }
 
+class MemoList: UIViewController {
+    
+    
+    @IBOutlet weak var noMemoLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        noMemoLabel.text = "메모가 없습니다.\n메모를 추가해주세요!"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+class AddMemo: UIViewController, UITextViewDelegate {
+
+    @IBOutlet weak var registerButton: UIBarButtonItem!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var contentsTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        contentsTextView.delegate = self
+    }
+
+
+}
