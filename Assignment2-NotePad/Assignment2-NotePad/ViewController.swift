d27279ea4d0ceeb1fa838a6efbb2f8a2bf9c5a71
jysuhr <jun0.suhr@gmail.com>
2024-05-19 23:04:01 +0900

    기초 설정

---
+++ Assignment2-NotePad/Assignment2-NotePad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2-NotePad
+//
+//  Created by 서준영 on 5/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
